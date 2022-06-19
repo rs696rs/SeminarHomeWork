f0cf9055e198fb530175ead8066a45017a87fb9b
«rs696» <rs696@yandex.ru>
2022-06-19 20:20:02 +0300

    task 2 is done

---
+++ Seminar1/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int n1, n2;
+
+Console.Write("Введите первое число: ");
+n1=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+n2=Convert.ToInt32(Console.ReadLine());
+
+if (n1>n2)
+{
+    Console.WriteLine("Большее чиcло "+n1);
+    Console.WriteLine("Меньшее чиcло "+n2);
+}
+
+else
+{
+Console.WriteLine("Большее чиcло "+n2);
+Console.WriteLine("Меньшее чиcло "+n1);
+}
