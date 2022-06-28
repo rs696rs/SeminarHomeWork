e7dc065c567dcd4c2d9cb7ae0bd5fc57d7db40b6
«rs696» <rs696@yandex.ru>
2022-06-28 20:44:43 +0300

    task 15 is done

---
+++ Seminar2/task15/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void Weekend(int m)
+{
+    if (m<6&m>0)
+    {
+    Console.WriteLine("Сегодня не выходной");
+    }
+    if (m>7)
+    {
+    Console.WriteLine("В неделе 7 дней");
+    }
+    if (m<=0)
+    {
+    Console.WriteLine("В неделе 7 дней");
+    }
+    while ((m>5)&&(m<8))
+    {
+    Console.WriteLine("Сегодня выходной)");
+    break;
+    }
+}
+
+Console.Write("Введите число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+Weekend(digit);
