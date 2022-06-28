f7705ceb28189ddaff47ee9a3f2e0d4072639e0c
«rs696» <rs696@yandex.ru>
2022-06-28 20:29:18 +0300

    task 13 is done

---
+++ Seminar2/task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+void ThirdyDigital(int m)
+{
+    if (m/100<=0)
+    {
+    Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+    int c=m%10;
+    Console.WriteLine("Третья цифра " + c);
+    }
+}
+
+Console.Write("Введите трехзначное число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+ThirdyDigital(digit);
