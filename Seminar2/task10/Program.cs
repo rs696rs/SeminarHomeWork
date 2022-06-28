b10d5ec4f79203d7b6c81925e1a6ce0bf824da8c
«rs696» <rs696@yandex.ru>
2022-06-28 20:24:57 +0300

    add folder Seminar 2

---
+++ Seminar2/task10/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+int SecondDigit(int n)
+{
+    int dec = n / 10 % 10;
+    return dec;
+}
+
+Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int SecD = SecondDigit(a); 
+Console.WriteLine("Вторая цифра " + SecD);
+
+
+
+
+
+
+
+
+
+    
+   
