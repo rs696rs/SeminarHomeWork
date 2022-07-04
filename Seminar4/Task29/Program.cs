9392f8899afb6547db006bca2b75f4d0a28e8050
«rs696» <rs696@yandex.ru>
2022-07-04 20:37:53 +0300

    task 29 is don't done

---
+++ Seminar4/Task29/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+void AddMasiv(int[] masiv)
+{
+    int length = masiv.Length;
+    int index = 0;
+    while (index < length)
+    {
+        masiv[index] = new Random().Next(1, 10);
+        index++;
+    }
+
+}
+
+void PrintMasiv(int[] mas)
+{
+    int count = mas.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.Write(mas[position]+",");
+        position++;
+    }
+    
+}
+
+Console.Write("Вчедите число, обозначающее длинну массива: ");
+int a=Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[a];
+
+AddMasiv(array);
+
+PrintMasiv(array);
+
+
+
