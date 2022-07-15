5b9671bedbaaa16b6fae6be6526a96fa0ef4cd06
«rs696» <rs696@yandex.ru>
2022-07-15 18:36:01 +0300

    task 47 is done

--- Seminar7/task47/Program.cs
+++ Seminar7/task47/Program.cs
@@ -4,14 +4,14 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-double [,] CreatRandom2dArray(int rows, int columns, int minValue, int maxValue)
+double[,] CreatRandom2dArray(int rows, int columns, int minValue, int maxValue)
 {
-    double [,] newArray = new double[rows, columns];
+    double[,] newArray = new double[rows, columns];
 
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < columns; j++)
-            newArray[i, j] = Math.Round(new Random().Next(minValue, maxValue + 1)+ new Random().NextDouble(), 2);
+            newArray[i, j] = Math.Round(new Random().Next(minValue, maxValue + 1) + new Random().NextDouble(), 2);
     }
     return newArray;
 }
@@ -21,9 +21,9 @@ void Show2dArray(double[,] array)
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
-            Console.Write(array[i,j] + " ");
+            Console.Write(array[i, j] + " ");
 
-        Console.WriteLine();    
+        Console.WriteLine();
     }
 }
 
@@ -42,6 +42,6 @@ int maxValue = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("");
 
 
-double[,] my2dArray = CreatRandom2dArray(rows,columns,minValue,maxValue);
+double[,] my2dArray = CreatRandom2dArray(rows, columns, minValue, maxValue);
 
 Show2dArray(my2dArray);
