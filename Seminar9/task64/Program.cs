3b5b02eb955247ca964bd3c7c651694ec28eb163
«rs696» <rs696@yandex.ru>
2022-07-25 17:08:59 +0300

    task 64 is done

---
+++ Seminar9/task64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
+// N = 4532 -> 4
+
+double NumOfDigit(double a)
+{
+    double count = 0;
+    if (a < 10) return 1;
+    else return count = 1 + NumOfDigit(a / 10);
+}
+
+Console.Write("Введите число: ");
+double num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(NumOfDigit(num));
