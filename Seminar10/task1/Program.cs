80372c053c59f7d0cd426dacf48cb1ac8ee0d4fa
«rs696» <rs696@yandex.ru>
2022-07-28 22:12:39 +0300

    add folder seminar 10

---
+++ Seminar10/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте массив строк. Напишите программу, считает кол-во слов в массиве, 
+// начинающихся на гласную букву.
+// Пример: { "qwe", "wer", "ert", "rty", "tyu"} -> 1
+
+void FindVowelLetter(string[] words)
+{
+    int count = 0;
+    string[] vovel = { "a", "e", "i", "o", "u", "y" };
+    for (int i = 0; i < words.Length; i++)
+        for (int j = 0; j < vovel.Length; j++)
+        {
+            if (words[i][0] == vovel[j][0]) count++;
+        }
+    Console.WriteLine("Количество слов в массиве, начинающихся на гласную букву: " + count);
+
+}
+
+string[] words = { "qwe", "wer", "ert", "rty", "tyu", "ouy", "ger", "min", "yes" };
+
+
+FindVowelLetter(words);
+
+
+
+
---
+++ Seminar10/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте массив строк. Напишите программу, которая генерирует новый массив, объединяя элементы исходного массива попарно.
+// Пример: { "qwe", "wer", "ert", "rty", "tyu", "yui"} -> { "qwewer", "ertrty", "tyuyui"}
+
+
+string[] CombiningWords(string[] words)
+{
+    string[] combining = new string[words.Length / 2];
+    int j = 0;
+    for (int i = 0; i <= words.Length - 2; i = i + 2)
+    {
+        combining[j] = words[i] + words[i + 1];
+        j++;
+    }
+    return combining;
+}
+
+void PrintWords(string[] words)
+{
+    for (int i = 0; i < words.Length; i++)
+    {
+        Console.Write(words[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] words = { "Доб", "рый", "ве", "чер", "Па", "вел,", "без ", "ваших", "семи", "наров", "ску", "чно!" };
+
+string[] NewWords = CombiningWords(words);
+
+PrintWords(NewWords);
